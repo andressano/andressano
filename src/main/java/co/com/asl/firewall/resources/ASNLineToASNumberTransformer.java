70f80db16a6355c2d94c2401f4b0d315899d66a4
Andres Sanchez <andressano@gmail.com>
2022-02-12 21:25:12 -0500

    CIDR Pattern adjustment

--- src/main/java/co/com/asl/firewall/resources/ASNLineToASNumberTransformer.java
+++ src/main/java/co/com/asl/firewall/resources/ASNLineToASNumberTransformer.java
@@ -2,13 +2,16 @@ package co.com.asl.firewall.resources;
 
 import co.com.asl.firewall.command.Command;
 import co.com.asl.firewall.entities.CIDRAddressV4;
-import co.com.asl.firewall.ip.CIDRPredicate;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 import org.springframework.util.CollectionUtils;
 
 public final class ASNLineToASNumberTransformer {
+
+  private static final String IP_NUMBER_PATTERN = "([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])";
+  private static final String CIDR_PATTERN = "^(" + IP_NUMBER_PATTERN
+      + "\\.){3}" + IP_NUMBER_PATTERN + "(\\/([0-9]|[1-2][0-9]|3[0-2]))$";
 
   private ASNLineToASNumberTransformer() {
     super();
@@ -24,7 +27,7 @@ public final class ASNLineToASNumberTransformer {
 
     return result
         .stream()
-        .filter(CIDRPredicate.IS_CIDR_PREDICATE)
+        .filter(l -> l.matches(CIDR_PATTERN))
         .map(CIDRAddressV4::new)
         .collect(Collectors.toList());
   }
