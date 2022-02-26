2e1c3f10435696ead94a449ce98c0e3635e5022d
Andres Sanchez <andressano@gmail.com>
2022-02-26 09:57:53 -0500

    Adding of new Russian hosts

--- src/main/java/co/com/asl/firewall/ip/concurrent/ASNWhoisCallable.java
+++ src/main/java/co/com/asl/firewall/ip/concurrent/ASNWhoisCallable.java
@@ -6,11 +6,14 @@ import co.com.asl.firewall.entities.CIDRAddressV4;
 import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+@Slf4j
 @Scope("prototype")
 @Component
 public class ASNWhoisCallable implements Callable<ASNumber> {
@@ -26,12 +29,16 @@ public class ASNWhoisCallable implements Callable<ASNumber> {
 
   @Override
   public ASNumber call() throws Exception {
-    return whoisQueries.stream()
+    ASNumber result = whoisQueries.stream()
         .map(q -> String.format(q, asn))
         .flatMap(Command::execute)
         .map(String::trim)
         .filter(CIDRAddressV4.PREDICATE)
         .map(CIDRAddressV4::new)
         .collect(Collectors.toCollection(() -> new ASNumber(asn)));
+    if (CollectionUtils.isEmpty(result)) {
+      log.warn("{} has no records", this.asn);
+    }
+    return result;
   }
 }
